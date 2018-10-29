180e045745906e095c8711a7c734250d2d8352c7
Nathan Burgo <nthnbrg185@gmail.com>
2018-10-28 20:44:53 -0600

    Added basic component structure for posts

---
+++ src/components/Postform.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class Postform extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            body: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    render() {
+        
+        return (
+            <div>
+                <h1>Add post</h1>
+                <form>
+                    <div>
+                        <label>Title: </label><br />
+                        <input type="text" name="title" value={this.state.title} onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label>Body: </label><br />
+                        <textarea name="body" value={this.state.body} onChange={this.onChange} />
+                    </div>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Postform;
